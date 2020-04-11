973c39078c4749c6288bf6dfa59bd1a67bdfd7b0
Néstor Andrés Peña <nestor2509@gmail.com>
2020-04-11 06:12:32 -0500

    avances con los totales de tests en colombia

--- public/js/script.js
+++ public/js/script.js
@@ -6,123 +6,64 @@ var xScale = d3.scaleLinear()
 .domain([0,35])
 .range([50,1900]);
 
+var colorScale = d3.scaleOrdinal()
+  .range(d3.schemeCategory10);
+
+var heightMax = 2000;
+var heightMaxLog = 2000;
+var dias;
+var dias_col = 0;
+
 Promise.all([
-  d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+  d3.csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'),
+  d3.json('./archivo.json'),
+  d3.csv('./totales_tests.csv')
 ]).then(files => {
   const data = files[0];
 
-  let dias = data.columns;
+  dias = data.columns;
   dias = dias.slice(4, dias.length);
-
-  let color = d3.scaleOrdinal()
-  .range(d3.schemeCategory10);
 
   var svg = d3.select('#bg');
   var svg_nuevos = d3.select('#bg_2');
+  var svg_paises_acumulado = d3.select('#bg_3');
+  var svg_totales_tests = d3.select('#bg_4');
+
   var gGraf = svg.append('g').attr('class', 'gGraf');
 
-  for(var i = 1; i < 2600; i+=200){
-    svg.append("line")
-      .attr('x1', d => {
-        return 50;
-      })
-      .attr('x2', d => {
-        return 1920;
-      })
-      .attr('y1', d => {
-        return heightScale(i);
-      })
-      .attr('y2', d => {
-        return heightScale(i);
-      })
-      .attr('stroke', '#dcdcdc')
-  }
-
-  for(var i = 1; i < 35; i++){
-    svg.append("line")
-      .attr('x1', d => {
-        return xScale(i);
-      })
-      .attr('x2', d => {
-        return xScale(i);
-      })
-      .attr('y1', d => {
-        return 0;
-      })
-      .attr('y2', d => {
-        return 800;
-      })
-      .attr('stroke', '#dcdcdc')
-  }
+  
 
   const newData = [];
+  const countryData = [];
 
   for(var i = 0; i < data.length; i++) {
     if(
     data[i]['Country/Region'] == 'Colombia'
-    // data[i]['Country/Region'] == 'Peru' || 
-    // data[i]['Country/Region'] == 'Argentina' || 
-    // data[i]['Country/Region'] == 'Chile' || 
-    // data[i]['Country/Region'] == 'Mexico' || 
-    // data[i]['Country/Region'] == 'Korea, South' || 
-    // data[i]['Country/Region'] == 'Egypt' || 
-    // // data[i]['Country/Region'] == 'China' ||
-    // data[i]['Country/Region'] == 'Iran' ||
-    // data[i]['Country/Region'] == 'Italy' ||
-    // data[i]['Country/Region'] == 'Brazil' ||
-    // data[i]['Country/Region'] == 'Ecuador'
-    ) newData.push(data[i])
+    ) {
+      newData.push(data[i])
+      countryData.push(data[i])
+    }
+    else if(
+      data[i]['Country/Region'] == 'Peru' || 
+      data[i]['Country/Region'] == 'Argentina' || 
+      data[i]['Country/Region'] == 'Chile' || 
+      data[i]['Country/Region'] == 'Mexico' || 
+      data[i]['Country/Region'] == 'Italy' ||
+      data[i]['Country/Region'] == 'Brazil' ||
+      data[i]['Country/Region'] == 'Ecuador'
+    ) countryData.push(data[i])
   }
 
   for(var i = 0; i < newData.length; i++) {
-    var index = 0;
-    var dataTemp = [];
+    var dataTemp = organizaData(newData[i]);
 
+    dias_col = dataTemp.length;
 
-    // svg.append('text')
-    // .text(newData[i]['Country/Region'])
-    // .attr('x',i*150 + 200)
-    // .attr('y', 900)
-    // .attr('font-size', '28px')
-    // .attr('font-weight', 'bold')
-    // .attr('fill', d => {
-    //   if(newData[i]['Country/Region']== 'Colombia') return 'rgb(237, 194, 42)';
-    //   else return color(newData[i]['Country/Region'])
-    // })
-    // .attr('text-anchor', 'middle')
+    xScale.domain([0,dias_col])
+    heightMax = parseInt(dataTemp[dias_col - 1].num) + 600;
+    heightMaxLog = parseInt(dataTemp[dias_col - 1].num) + 3000;
 
-    for(var j = 0; j < dias.length; j++) {
-      if(newData[i][dias[j]] > 0){
-        var cambio = 0;
-        var dif = 0;
-        var r = 0;
-        if(dataTemp.length > 0){
-          cambio = ((newData[i][dias[j]] - newData[i][dias[j-1]]) / newData[i][dias[j-1]]);
-          dif = ((newData[i][dias[j]] - newData[i][dias[j-1]]));
-          var rTemp = 0;
-          for(var x = 0; x < dataTemp.length; x++){
-            rTemp += dataTemp[x].cambio;
-          }
-
-          rTemp += cambio;
-          r = rTemp / (dataTemp.length + 1)
-        }
-
-        
-
-        var obj = {
-          num: newData[i][dias[j]],
-          name: newData[i]['Country/Region'],
-          cambio: cambio,
-          dif: dif,
-          r: r
-        }
-        if(index < dias.length) dataTemp.push(obj);
-        else if(j < dias.length) dataTemp.push(obj);
-        index++;
-      }
-    }
-
+    heightScale.domain([heightMax,0]).range([0,800])
     dibujarAxis(svg, true);
 
     // Curva log
@@ -135,35 +76,113 @@ Promise.all([
     heightScale.domain([350,0]).range([0,300])
     dibujarAxis(svg_nuevos, false);
     heightScale.domain([350,0]).range([0,300])
+       
 
+    var data_regre = dataTemp.slice(dataTemp.length - 7, dataTemp.length)
+
+    var x_mean = 0;
+    var y_mean = 0;
+
+    for(var m = 0; m < data_regre.length; m++){
+      x_mean += m;
+      y_mean += data_regre[m].dif;
+    }
+
+    x_mean = x_mean / data_regre.length
+    y_mean = y_mean / data_regre.length
+
+    var sqrt_sub = 0;
+    var sum_sub = 0;
+
+    for(var m = 0; m < data_regre.length; m++){
+      var x_sub = m -  x_mean
+      var y_sub = data_regre[m].dif -  y_mean
+      sqrt_sub += x_sub * x_sub
+      sum_sub += x_sub * y_sub
+    }
+
+    var pendiente = sum_sub / sqrt_sub
+    var off = y_mean - (pendiente * x_mean)
+    console.log(pendiente,off)
+
+    var x1 = dataTemp.length - 7;
+    var x2 = dataTemp.length - 1;
+    var y1 = 106;
+    var y2 = 6 * pendiente + off;
+
+    console.log(y1,y2)
+
+    svg_nuevos.append('line')
+    .attr('stroke', 'red')
+    .attr('stroke-width', 2)
+    .attr('stroke-dasharray', 4)
+    .attr('x1', xScale(x1))
+    .attr('x2', xScale(x2))
+    .attr('y1', heightScale(y1))
+    .attr('y2', heightScale(y2))
+
+    dibujarBarras(svg_nuevos, dataTemp, 'rect_1','rgba(237, 194, 42, 0.3)','dif')
     dibujarCurva(svg_nuevos, dataTemp, '#666', 'dif');
     dibujarPuntos(svg_nuevos, 'circles_2', dataTemp, '#999', 30, 'dif', true);
 
-    var rect = svg_nuevos.selectAll('.rect')
-    .data(dataTemp)
+    heightScale.domain([3500,0]).range([0,300])
+    dibujarAxis(svg_totales_tests, false);
+    heightScale.domain([3500,0]).range([0,300])
+    for(var m = 0; m < files[2].length; m++){
+      var total = parseInt(files[2][m].total);
+      if(m > 0){
+        var anterior = parseInt(files[2][m - 1].total);
+        if(anterior > 0) files[2][m].dif = total - anterior
+        else files[2][m].dif = 0
+      } else {
+        files[2][m].dif = 0
+      }
+    }
+    dibujarBarras(svg_totales_tests, files[2], 'rect_1','rgba(237, 194, 42, 0.2)','dif')
+    dibujarBarras(svg_totales_tests, dataTemp, 'rect_1','rgba(237, 194, 42, 0.4)','dif')
+
+    console.log(files[2], 'hola')
+
+    // Curva acumulado
+    heightScale.domain([heightMax,0]).range([0,800])
+    dibujarCurva(svg, dataTemp, 'rgb(237, 194, 42)', 'num');
+    dibujarPuntos(svg, 'circles', dataTemp, 'rgb(237, 194, 42)', -20, 'num', false);
+  }
+
+  heightScale.range([0,500])
+  dibujarAxis(svg_paises_acumulado, false);
+  
+  for(var i = 0; i < countryData.length; i++) {
+    var dataTemp = organizaData(countryData[i]);
+
+    console.log(dataTemp)
+
+    heightScale.domain([heightMax,0]).range([0,500])
+    dibujarCurva(svg_paises_acumulado, dataTemp, 'rgb(237, 194, 42)', 'num');
+    dibujarDot(svg_paises_acumulado,'dots_'+i, dataTemp, 'num');
+  }
+})
+
+function dibujarBarras(svg, data, clase,color, key) {
+  var rect = svg.selectAll('.' + clase)
+    .data(data)
     .join('g')
-    .attr('class', 'rect')
+    .attr('class', clase)
     .attr('transform', (d,i)=>{
       return 'translate('+xScale(i)+',0)'
     })
 
     rect.append('rect')
-    .attr('fill', 'rgba(0,0,0,0.2)')
+    .attr('fill', color)
     .attr('x', -9)
     .attr('y', d=>{
-      return heightScale(d.dif)
+      return heightScale(d[key])
     })
     .attr('width', 18)
     .attr('height', d=>{
-      return 300 - heightScale(d.dif)
+      return 300 - heightScale(d[key])
     })
-
-    // Curva acumulado
-    heightScale.domain([2600,0]).range([0,800])
-    dibujarCurva(svg, dataTemp, 'rgb(237, 194, 42)', 'num');
-    dibujarPuntos(svg, 'circles', dataTemp, 'rgb(237, 194, 42)', -20, 'num', false);
-  }
-})
+}
 
 function dibujarCurva(svg, data, color, key) {
   svg.append("path")
@@ -171,17 +190,35 @@ function dibujarCurva(svg, data, color, key) {
   .attr("fill", "none")
   .attr("stroke", (d) => {
     if(d[0].name == 'Colombia') return color;
-    else return color(d[0].name)
+    else return colorScale(d[0].name)
   })
   .attr("stroke-width", d => {
     if(d[0].name == 'Colombia') return 3;
-    else return 1.5;
+    else return 1;
   })
   .attr("d", d3.line()
     .curve(d3.curveMonotoneX)
     .x(function(d,i) { return xScale(i) })
     .y(function(d) { return heightScale(d[key]) })
   )
+}
+
+function dibujarDot(svg, clase, data, key){
+  var nodos = svg.selectAll('.'+clase)
+    .data(data)
+    .join('g')
+    .attr('class', clase)
+    .attr('transform', (d,i)=>{
+      return 'translate('+xScale(i)+','+heightScale(d[key])+')'
+    })
+
+    nodos.append('circle')
+    .attr('r', 2)
+    .attr("fill", (d) => {
+      if(d.name == 'Colombia') return 'rgb(237, 194, 42)';
+      else return colorScale(d.name)
+    })
+    .attr('stroke', 'none')
 }
 
 function dibujarPuntos(svg, clase, data, color, pos, key, por) {
@@ -221,6 +258,40 @@ function dibujarPuntos(svg, clase, data, color, pos, key, por) {
 }
 
 function dibujarAxis(svg, log) {
+  for(var i = 1; i < heightScale.domain()[0]; i+=200){
+    svg.append("line")
+      .attr('x1', d => {
+        return 50;
+      })
+      .attr('x2', d => {
+        return xScale.range()[1];
+      })
+      .attr('y1', d => {
+        return heightScale(i);
+      })
+      .attr('y2', d => {
+        return heightScale(i);
+      })
+      .attr('stroke', '#dcdcdc')
+  }
+
+  for(var i = 1; i < dias_col; i++){
+    svg.append("line")
+      .attr('x1', d => {
+        return xScale(i);
+      })
+      .attr('x2', d => {
+        return xScale(i);
+      })
+      .attr('y1', d => {
+        return 0;
+      })
+      .attr('y2', d => {
+        return heightScale.range()[1];
+      })
+      .attr('stroke', '#dcdcdc')
+  }
+
   svg.append("g")
   .attr("transform", "translate(50,0)")
   .call(d3.axisLeft(heightScale));
@@ -243,12 +314,49 @@ function dibujarAxis(svg, log) {
 function escalaLogaritmica() {
   heightScale = d3.scaleSymlog()
     .constant(1)
-    .domain([4000,0])
+    .domain([heightMaxLog,0])
     .range([0,800]);
 }
 
 function escalaLineal() {
   heightScale = d3.scaleLinear()
-    .domain([2600,0])
+    .domain([heightMax,0])
     .range([0,800]);
+}
+
+function organizaData(newData) {
+  var dataTemp = [];
+  var index = 0;
+
+  for(var j = 0; j < dias.length; j++) {
+    if(newData[dias[j]] > 0){
+      var cambio = 0;
+      var dif = 0;
+      var r = 0;
+      if(dataTemp.length > 0){
+        cambio = ((newData[dias[j]] - newData[dias[j-1]]) / newData[dias[j-1]]);
+        dif = ((newData[dias[j]] - newData[dias[j-1]]));
+        var rTemp = 0;
+        for(var x = 0; x < dataTemp.length; x++){
+          rTemp += dataTemp[x].cambio;
+        }
+
+        rTemp += cambio;
+        r = rTemp / (dataTemp.length + 1)
+      }
+
+      var obj = {
+        num: newData[dias[j]],
+        name: newData['Country/Region'],
+        cambio: cambio,
+        dif: dif,
+        r: r
+      }
+      if(index < dias.length) dataTemp.push(obj);
+      else if(j < dias.length) dataTemp.push(obj);
+      index++;
+    }
+  }
+
+  return dataTemp;
 }
